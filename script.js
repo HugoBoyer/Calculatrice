fff370e16bcf850694d79881c3bdb043ca1f40c9
Hugo <boyer.hugo974@gmail.com>
2025-09-27 01:25:01 +0200

    feat: construction de la function calculate

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
-/*let buttonClick = document.querySelectorAll('button');
-let display = document.querySelector('.display');
-
-console.log(buttonClick);
-
-buttonClick.addEventListener('click', (e) => {
-     alert('Bouton cliqué !');
-})
-
-document.addEventListener('keydown', function(event) {
-    console.log('Touche pressée:', event.key);
-});
-
-
-*/
 console.log("Le script est bien connecté !");
 
 let inputDisplay = document.querySelector('input');
 let buttonClick = document.querySelectorAll('button');
+let buttonEgal = document.getElementById('ButtonEgal');
+// stocke toutes ce que l'utilisateur tape
+let expression = '';
 
 buttonClick.forEach(button => {
     button.addEventListener('click', (e) => {
-        value = e.target.textContent;
+        let value = e.target.textContent;
+        expression += value;
         inputDisplay.value += value;
         console.log(e.target.textContent);
+        
+        if(value === 'C'){
+            expression = '';
+            inputDisplay.value = '';
+        }
     })
 })
+
+
+
+
+buttonEgal.addEventListener('click', () => {
+
+}
+
+
+function calculate(expression) {
+    // Separer les nombre et les operateurs
+    const tokens = expression.match(/\d+(\.\d+)?|[+\-*/]/g)
+    console.log(tokens);
+
+
+    // Gerer la prioriter des operation * et /
+    let stack = [];
+    let i = 0;
+    while (i < tokens.length) {
+        let token = tokens[i]
+        if(token === "*" || token === "/") {
+            const prev = parseFloat(stack.pop());
+            const next = parseFloat(tokens[i + 1]); // le nombre suivant
+            stack.push(token === "*" ? prev * next : prev / next);
+            i += 2;
+        }else {
+        stack.push(token);
+        i++;
+    }
+} 
+
+
+// Faire les addition et soustraction
+
+return res
+
+})
+
+
